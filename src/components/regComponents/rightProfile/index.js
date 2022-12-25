87564e29d26a70fd3f36aac03f4f01e57b83404c
Vladimir <felps0308@gmail.com>
2022-12-25 12:07:47 +0300

    fixed current user info details showing

--- src/components/regComponents/rightProfile/index.js
+++ src/components/regComponents/rightProfile/index.js
@@ -4,6 +4,11 @@ import { useDispatch } from "react-redux";
 import { signOutUserStart } from "../../../redux/User/user.actions";
 import { Alert, Button } from "reactstrap";
 
+// connect with firestore db
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { firestore } from '../../../firebase/config';
+
+// for takeing user
 import { useAuth } from "../../../redux/User/user.sagas";
 
 //style and images
@@ -11,50 +16,68 @@ import Image1 from '../../images/images.png';
 import './style.css'
 
 function RightProfile() {
-    const { currentUser } = useAuth()
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [error, setError] = useState("");
   
-    async function signOut() {
-      setError("");
-      try {
-        dispatch(signOutUserStart());
-        history.push("/login");
-      } catch {
-        setError("Failed to log out");
+  const { currentUser } = useAuth()
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [error, setError] = useState("");
+  
+  // taking users from firestore
+  const usersRef = firestore.collection('users');  
+  const [users] = useCollectionData(usersRef, { idField: 'id' }); 
+
+  // find amd past current user info
+  if(users){    
+    for (let i = 0; i < users.length; i++) {
+      if(users[i].id === currentUser.uid){
+        var userName = users[i].displayName
+        var userImg  = users[i].photoURL   
       }
     }
-    return (
-        <div className="rightProfile">
-            {currentUser.photoURL ?
-              <img style={{borderRadius: "50%"}}
-                   src={currentUser.photoURL} alt="Admin"
-                   width="130" height="130" />
-              : <img style={{borderRadius: "50%"}}
-                     src={Image1} alt="Admin"
-                     width="130" height="130" />}
-          {/* photoUrl exist only with entering through google or social media... */}
+  }
+  
+  // signout part
+  async function signOut() {
+    setError("");
+    try {
+      dispatch(signOutUserStart());
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
 
-          {currentUser.displayName ? <h6> {currentUser.displayName }</h6> : <h6>Admin</h6>}
-          {/* it's grab info from google account but inside your form it's useless */}
+  return (
+    <div className="rightProfile">
+      {users ?
+        <img style={{borderRadius: "50%"}}
+          src={userImg} alt="Admin"
+          width="130" height="130" />
+        : <img style={{borderRadius: "50%"}}
+          src={Image1} alt="Admin"
+          width="130" height="130" />} 
 
-          <h6><i style={{color: "gray"}} className="far fa-envelope"/> {currentUser.email}</h6>
-
-          <hr/>
-
-          <Button className="logoutBtn" onClick={() => signOut()}>
-            Log out <i className="fas fa-sign-out-alt" />
-          </Button>
-          {error && <Alert color="danger"> {error} </Alert>}
-          <div className="updateProfile">
-            <Link to="/update" style={{ color: "#000" }}>
-              change password
-            </Link>
-          </div>
-            
-        </div>
-    )
+      {users ? <h6> {userName}</h6> : <h6>Admin</h6>}
+      
+      <h6>
+        <i style={{color: "gray"}} className="far fa-envelope"/> {currentUser.email}
+      </h6>
+      <hr/>
+      
+      <Button className="logoutBtn" onClick={() => signOut()}>
+        Log out <i className="fas fa-sign-out-alt" />
+      </Button>
+      {error && <Alert color="danger"> {error} </Alert>}
+      
+      <div className="updateProfile">
+        <Link to="/update" style={{ color: "#000" }}>
+          change password
+        </Link>
+      </div>
+        
+    </div>
+  )
 }
 
 export default RightProfile
