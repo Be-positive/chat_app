1a8faac90bf7e467985e896d76a818c5349e90d3
Vladimir <felps0308@gmail.com>
2022-12-27 15:00:14 +0300

    add additional users list for mobile version and styling it

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,7 +15,12 @@ const DashBoard = () => {
 
   const [showProfileInfo, setShowProfileInfo] = useState(false);
   const [ showCloseBtn, setShowCloseBtn] = useState(false)
+
+  const [showUsersInfo, setShowUsersInfo] = useState(false);
+  const [showCloseUsersBtn, setshowCloseUsersBtn] = useState(false)
+
   
+  // SHOWED UP and CLOSED PROFILE INFO WHEN WINDOW SIZE LESS THEN 1100
   const showProfile = (e) => {  
     e.stopPropagation()
     setShowProfileInfo(true)
@@ -26,7 +31,20 @@ const DashBoard = () => {
     setShowProfileInfo(false) 
     setShowCloseBtn(false)
   }
-  
+
+  // SHOWED UP and CLOSED USERS LIST WHEN WINDOW SIZE LESS THEN 820
+  const showUsers = (e) => {  
+    e.stopPropagation()
+    setShowUsersInfo(true)
+    setshowCloseUsersBtn(true)                                 
+  }
+  const hideUsers = (e) => {
+    e.stopPropagation()       
+    setShowUsersInfo(false)
+    setshowCloseUsersBtn(false)
+  }
+
+  // CHECK LOADING STATE
   const [loading, setLoading] = useState(true);
   
   useEffect(() => {
@@ -35,30 +53,45 @@ const DashBoard = () => {
     }, 3000);
   }, []);
 
+  // SHOWED UP MAIN CHAT COMPONENT AND HIDE MAIN USERS LIST COMPONENT 
   function chatProfile(){
-    let chatProfile = document.getElementById('chatProfile')
-    chatProfile.style.display = "flex";
+    var leftSide = document.getElementById('leftSide')
+    var chatProfile = document.getElementById('chatProfile')
+    chatProfile.style.display = "flex"; 
+    
+    if(window.innerWidth <= 820){      
+      leftSide.style.display = "none";
+    }     
   }
-  
+
   return (
     
     <section id="dashboard">
       {loading && <Spinner />}      
       <center className="mainLand">
 
-        <div className="leftSide" onClick={chatProfile} >
+        <div className="leftSide" id="leftSide" onClick={chatProfile} >
           <SearchLayout />
         </div>
 
         <div className="center" id="chatProfile" style={{display: "none"}}>
           <div className="profileInfo">
+            <Button  id='usersBtn' onClick={showUsers} color="primary" size="lg" className="usersBtn">Users</Button>
+            <div id='usersList' style={{transform: showUsersInfo ? "translate3d(0vw, 0, 0)" : "translate3d(-200vw, 0, 0)"}}>
+              <button id='closeUsers' style={{transform: showCloseUsersBtn ? "translate3d(0vw, 0, 0)" : "translate3d(-200vw, 0, 0)"}} onClick={hideUsers}>CLOSE</button>
+              <SearchLayout /> 
+            </div>
+
             <Button  id='profileBtn' onClick={showProfile}  className="profileBtn">Profile <i className="fas fa-user"></i> </Button>
             <div id='profileAdmin' style={{transform: showProfileInfo ? "translate3d(0vw, 0, 0)" : "translate3d(200vw, 0, 0)"}}>
               <button id='closeProfile' style={{transform: showCloseBtn ? "translate3d(0vw, 0, 0)" : "translate3d(200vw, 0, 0)"}} onClick={closeProfile}>CLOSE</button>
               <RightProfile /> 
             </div>
-          </div>            
-          <CenterChat/> 
+            
+          </div>    
+          <div onClick={() => {setShowUsersInfo(false); setShowProfileInfo(false)}}>
+            <CenterChat/>            
+          </div>                   
         </div>
 
         <div className="rightSide" >
